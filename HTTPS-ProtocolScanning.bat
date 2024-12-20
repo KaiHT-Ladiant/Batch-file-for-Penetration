be0e861b69f194107b986ece4e562871dd56fd38
Kai_HT <157885033+KaiHT-Ladiant@users.noreply.github.com>
2024-12-20 11:32:40 +0900

    Create HTTPS-ProtocolScanning.bat

---
+++ HTTPS-ProtocolScanning.bat
@@ -0,0 +1,39 @@
+@echo off
+
+REM Scan Web Application HTTPS Protocols Alghorithm
+REM Code By Kai_HT
+
+REM Will be updated later
+REM Version 1.0
+REM Set Encoding Type "ASCII" to save
+
+REM Nmap installation is required.
+
+title HTTPS Protocol Scanning - Kai_HT
+echo ┌───────────────────────────────────────────────────┒
+echo │               HTTPS Ciphers Scanning              ┃
+echo │               Scripting From Kai_HT               ┃
+echo ┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+echo.
+echo.
+echo ─────────
+set /p domainName=What is scanning target? (Input Domain Name): 
+echo.
+echo.
+echo ─────────
+echo Scanning All Ready
+timeout 3
+echo.
+echo.
+echo.
+echo ─────────
+echo Scanning Start
+echo.
+echo.
+echo [WEB-SER-035] %domainName% Protocols Scanning
+nmap --script ssl-enum-ciphers %domainName%
+echo.
+echo.
+echo ───────────────────────────────
+echo HTTPS Protocol Scanning is End.
+pause
